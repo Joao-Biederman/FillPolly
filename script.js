712c1b53aa794ce9b0bec5e08b704a7d56effe30
Joao-Biederman <joaobiedetman@gmail.com>
2024-01-31 21:46:02 -0300

    FillPolly working, no color changing on vertex.

--- script.js
+++ script.js
@@ -1,153 +1,302 @@
-const getTheme = () => {
-  const theme = localStorage.getItem('theme');
-  theme && setTheme(theme);
+const canvas = document.getElementById('fill_polly_canvas');
+const context = canvas.getContext("2d");
+
+const delete_all_button = document.getElementById('delete-all-btn');
+const triangle_list = document.getElementById('triangles');
+
+let triangle_count = 0;
+let dots_count = 0;
+let triangles = [];
+
+function clear_canvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height)
 }
 
-const setTheme = (theme) => {
-  document.documentElement.className = theme;
-  localStorage.setItem('theme', theme);
+function random_color() {
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+
+  return {
+    r: r,
+    g: g,
+    b: b
+  };
 }
 
-const changeTheme = () => {
-  
-  if (localStorage.getItem('theme') === 'light') {
-    setTheme('dark');
-  } else {
-    setTheme('light');
-  }
+function get_rgb(color) {
+  return 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')'
 }
 
-getTheme();
-
-const getDots = () => {
-  return JSON.parse(localStorage.getItem('dots'));
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
-const setDots = (dots) => {
-  localStorage.setItem('dots', JSON.stringify(dots));
+function rgbToHex(color) {
+  return "#" + componentToHex(color.r) + componentToHex(color.g) + componentToHex(color.b);
 }
 
-function deleteFirst() {
-  var dotList = getDots();
-  for (let i = 0; i < 3; i++) {
-    if (dotList.length < 1) {
-      break;
-    }
-    dotList.shift();
-  }
-  setDots(dotList);
-  
-  var canvas = document.getElementById('fill_polly_canvas');
-  var context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  
-  dotList.forEach(dot => {
-    drawDot(context, dot.x, dot.y, dot.color)
+function hexToRgb(hex) {
+  var bigint = parseInt(hex, 16);
+  var r = (bigint >> 16) & 255;
+  var g = (bigint >> 8) & 255;
+  var b = bigint & 255;
+
+  return (get_rgb(color = { r: r, g: g, b: b }));
+}
+
+
+function draw_triangles() {
+  triangles.forEach(triangle => {
+    triangle.fill_polly();
   });
-
-  
-  if (dotList.length >= 3) {
-    drawTriangles(context, dotList);
-  }
 }
 
-const deleteAll = () => {
-  let dotList = [];
-  setDots(dotList);
-
-  var canvas = document.getElementById('fill_polly_canvas');
-  var context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
+function remove_triangle(id) {
+  let index = 0;
+  triangles.forEach((triangle, i) => {
+    if (triangle.id == id) {
+      index = i;
+    }
+  });
+  triangles.splice(index, 1);
 }
 
-const drawDot = (context, x, y, color) => {
-  context.fillStyle = color;
-
-  context.beginPath();
-  context.arc(x, y, 4, 0, 2 * Math.PI);
-  context.fill();
+function delete_all_triangles() {
+  triangle_list.innerHTML = "";
+  triangles = [];
+  triangle_count = 0;
+  dots_count = 0;
+  clear_canvas();
 }
 
-const drawTriangles = (context, points) => {
-  context.lineWidth = 4;
-  
-  for (var i = 0; i < points.length - 2; i += 3) {
-    let gradient = context.createLinearGradient(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
-    gradient.addColorStop(0.5, points[i].color);
-    gradient.addColorStop(1, points[i+1].color);
-    drawLine(context, points[i], points[i+1], gradient)
-
-    gradient = context.createLinearGradient(points[i+1].x, points[i+1].y, points[i+2].x, points[i+2].y);
-    gradient.addColorStop(0.5, points[i+1].color);
-    gradient.addColorStop(1, points[i+2].color);
-    drawLine(context, points[i+1], points[i+2], gradient)
-
-    gradient = context.createLinearGradient(points[i].x, points[i].y, points[i+2].x, points[i+2].y);
-    gradient.addColorStop(0.5, points[i].color);
-    gradient.addColorStop(1, points[i+2].color);
-    drawLine(context, points[i], points[i+2], gradient)
-  }
-  
-  // context.lineWidth = 1;
+function redraw_canvas() {
+  clear_canvas();
+  draw_triangles();
 }
 
-const drawLine = (context, point1, point2, color) => {
-  context.strokeStyle = color;
-  context.beginPath();
-  context.moveTo(point1.x, point1.y);
-  context.lineTo(point2.x, point2.y);
-  context.stroke();
+function add_to_triangle_list(triangle) {
+  triangles.push(triangle);
+
+  const li_triangle = document.createElement("li");
+  const delete_button = document.createElement("button");
+
+  delete_button.innerText = "Delete";
+
+  delete_button.addEventListener("click", (event) => {
+    const closest_li = event.target.closest('li');
+    remove_triangle(closest_li.id);
+    triangle_list.removeChild(closest_li);
+    redraw_canvas();
+  })
+
+  let color0 = document.createElement("input");
+  color0.type = "color";
+  color0.value = rgbToHex(triangle.vertices[0].color);
+
+  let color1 = document.createElement("input");
+  color1.type = "color";
+  color1.value = rgbToHex(triangle.vertices[1].color);
+
+  let color2 = document.createElement("input");
+  color2.type = "color";
+  color2.value = rgbToHex(triangle.vertices[2].color);
+
+  li_triangle.setAttribute("id", triangle.id);
+  li_triangle.innerHTML = `Triangle ${triangle.id}`;
+  li_triangle.appendChild(color0);
+  li_triangle.appendChild(color1);
+  li_triangle.appendChild(color2);
+  li_triangle.appendChild(delete_button);
+  triangle_list.appendChild(li_triangle);
 }
 
-const mixColors = (color1, color2) => {
-  // Extract RGB components from color strings
-  var rgb1 = color1.match(/\d+/g).map(Number);
-  var rgb2 = color2.match(/\d+/g).map(Number);
-  
-  // Calculate the average of each component
-  var mixedColor = 'rgb(' +
-  Math.round((rgb1[0] + rgb2[0]) / 2) + ',' +
-  Math.round((rgb1[1] + rgb2[1]) / 2) + ',' +
-  Math.round((rgb1[2] + rgb2[2]) / 2) +
-  ')';
-    
-  return mixedColor;
+function paint(x, y, color) {
+  context.fillStyle = get_rgb(color);
+  context.fillRect(x, y, 1, 1);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var canvas = document.getElementById('fill_polly_canvas');
-            
-  var ctx = canvas.getContext("2d");
-
-  function Dot(x, y){
+class dot {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-  };
 
-  canvas.addEventListener("click", function (event) {
-    var x = event.clientX - canvas.getBoundingClientRect().left;
-    var y = event.clientY - canvas.getBoundingClientRect().top;
-    var color = getRandomColor();
-    var dotList = getDots();
-
-    dotList.push({x:x, y:y, color: color});
-
-    drawDot(ctx, x, y, color);
-
-    setDots(dotList);
-    
-    if (dotList.length >= 3) {
-      drawTriangles(ctx, dotList);
-    }
-  });
-
-  function getRandomColor() {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    
-    var rgbColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-    
-    return rgbColor;
+    this.color = random_color()
   }
+
+  set(color) {
+    this.color = color;
+    this.draw();
+  }
+
+  draw() {
+    context.fillStyle = get_rgb(this.color);
+
+    context.beginPath();
+    context.arc(this.x, this.y, 4, 0, 2 * Math.PI);
+    context.fill();
+  }
+}
+
+class triangle {
+  constructor(dot, id) {
+    this.id = id;
+    this.edge_color = `rgb(000,000,000)`;
+    this.vertices = [];
+    this.minY = Number.POSITIVE_INFINITY;
+    this.maxY = Number.NEGATIVE_INFINITY;
+    this.add_dot(dot);
+    this.intersection = new Map()
+  }
+
+  add_dot(new_dot) {
+    this.vertices.push(new_dot);
+    if (new_dot.y > this.maxY) this.maxY = new_dot.y;
+    if (new_dot.y < this.minY) this.minY = new_dot.y;
+    new_dot.draw();
+
+    if (this.vertices.length == 3) this.fill_polly();
+  }
+
+  set_edge_color(color) {
+    this.edge_color = color;
+  }
+
+  change_dot(color, id) {
+    this.vertices[id].set_color(color);
+  }
+
+  define_edges() {
+    for (let i = 0; i < 3; i++) {
+      let initialY, endY;
+      let currentX, currentColor;
+
+      const next_vertex = this.vertices[(i + 1) % 3];
+
+      let y_diference = next_vertex.y - this.vertices[i].y;
+
+      const variation = (next_vertex.x - this.vertices[i].x) / y_diference;
+      const variationR = (next_vertex.color.r - this.vertices[i].color.r) / y_diference;
+      const variationG = (next_vertex.color.g - this.vertices[i].color.g) / y_diference;
+      const variationB = (next_vertex.color.b - this.vertices[i].color.b) / y_diference;
+
+      if (this.vertices[i].y < next_vertex.y) {
+        initialY = this.vertices[i].y;
+        endY = next_vertex.y;
+        currentX = this.vertices[i].x;
+
+        currentColor = { r: this.vertices[i].color.r, g: this.vertices[i].color.g, b: this.vertices[i].color.b };
+      } else {
+        initialY = next_vertex.y;
+        endY = this.vertices[i].y;
+        currentX = next_vertex.x;
+
+        currentColor = { r: next_vertex.color.r, g: next_vertex.color.g, b: next_vertex.color.b };
+      }
+
+      for (let currentY = initialY; currentY < endY; currentY++) {
+        this.intersection.get(currentY).push({ x: currentX, color: currentColor });
+        currentX += variation;
+        let newColor = {
+          r: currentColor.r + variationR,
+          g: currentColor.g + variationG,
+          b: currentColor.b + variationB
+        };
+        currentColor = newColor;
+      }
+
+    }
+  }
+
+  draw_edges() {
+    context.strokeStyle = this.edge_color;
+    context.beginPath();
+    context.moveTo(this.vertices[0].x, this.vertices[0].y);
+    context.lineTo(this.vertices[1].x, this.vertices[1].y);
+    context.lineTo(this.vertices[2].x, this.vertices[2].y);
+    context.lineTo(this.vertices[0].x, this.vertices[0].y);
+    context.stroke();
+
+  }
+
+  draw_dots() {
+    this.vertices.forEach(dot => {
+      dot.draw();
+    });
+  }
+
+  sortIntersectionX() {
+    this.intersection.forEach((sortX) => {
+      const sortedX = sortX.slice().sort((a, b) => a.x - b.x);
+
+      sortX.splice(0, sortX.length, ...sortedX);
+    });
+  }
+
+  fill_polly() {
+    this.draw_dots();
+    for (let y = this.minY; y < this.maxY; y++) {
+      this.intersection.set(y, [])
+
+    }
+    this.define_edges();
+    this.sortIntersectionX();
+
+    for (let currentY = this.minY; currentY < this.maxY; currentY++) {
+      const current_line = this.intersection.get(currentY);
+      let k = 0;
+      let color = current_line[k].color;
+
+      do {
+        let firstX = Math.ceil(current_line[k].x);
+        let endX = Math.floor(current_line[k + 1].x);
+
+        const variationR = (current_line[k + 1].color.r - current_line[k].color.r) / (endX - firstX);
+        const variationG = (current_line[k + 1].color.g - current_line[k].color.g) / (endX - firstX);
+        const variationB = (current_line[k + 1].color.b - current_line[k].color.b) / (endX - firstX);
+        for (let currentX = firstX; currentX < endX; currentX++) {
+          paint(currentX, currentY, color);
+          let newColor = { r: color.r + variationR, g: color.g + variationG, b: color.b + variationB };
+          color = newColor;
+        }
+
+        k += 2;
+      } while (current_line[k]);
+    }
+  }
+}
+
+canvas.addEventListener("click", (event) => {
+  let rect = canvas.getBoundingClientRect()
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+
+  let new_dot = new dot(x, y);
+  if (dots_count === 0) {
+    new_triangle = new triangle(new_dot, triangle_count);
+
+    dots_count++;
+    triangle_count++;
+    return;
+  }
+
+  if (dots_count !== 0) {
+    new_triangle.add_dot(new_dot)
+
+    dots_count++;
+  }
+
+  if (dots_count == 3) {
+    new_triangle.draw_edges();
+    add_to_triangle_list(new_triangle);
+    dots_count = 0;
+    return
+  }
+
 });
+
+delete_all_button.addEventListener("click", (event) => {
+  delete_all_triangles();
+  return
+})
